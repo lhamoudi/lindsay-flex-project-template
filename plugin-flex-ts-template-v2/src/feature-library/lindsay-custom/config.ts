9a0b0a3691891424bb9c229089ef30fc5cc6cffc
Lindsay Hamoudi <lhamoudi@twilio.com>
2023-05-01 16:09:59 -0700

    Ditching the custom feature

--- plugin-flex-ts-template-v2/src/feature-library/lindsay-custom/config.ts
+++
@@ -1,7 +0,0 @@
-import { getFeatureFlags } from '../../utils/configuration';
-
-const { enabled = false } = getFeatureFlags()?.features?.lindsay_custom || {};
-
-export const isFeatureEnabled = () => {
-  return enabled;
-};
