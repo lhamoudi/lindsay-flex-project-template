8ab5f33cf103833be620d4242a7cb37094c100f8
Lindsay Hamoudi <lhamoudi@twilio.com>
2023-07-20 11:34:53 -0700

    branding

--- plugin-flex-ts-template-v2/src/feature-library/lindsay-branding/flex-hooks/css-overrides/index.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/lindsay-branding/flex-hooks/css-overrides/index.tsx
@@ -7,7 +7,7 @@ export const cssOverrideHook = (_flex: typeof Flex, _manager: Flex.Manager) => {
         background: '#000000', // Macquarie header (black)
         color: '#ffffff',
         'div:nth-of-type(2)>img': {
-          maxHeight: '50px',
+          maxHeight: '50px', // Macquarie logo size
         },
       },
     },
@@ -16,18 +16,18 @@ export const cssOverrideHook = (_flex: typeof Flex, _manager: Flex.Manager) => {
         background: '#2e323f', // Macquarie deselected (grey)
         '.Twilio-Side-Link': {
           '.Twilio-Side-Link-IconContainer': {
-            color: '#ffffff',
+            color: '#ffffff', // White icons
           },
         },
         '.Twilio-Side-Link:hover': {
           '.Twilio-Side-Link-IconContainer': {
-            color: '#000000',
+            color: '#000000', // Black icons on hover
           },
         },
         '.Twilio-Side-Link--Active': {
           background: '#4472c4 !important', // Macquarie selected (blue)
           '.Twilio-Side-Link-IconContainer': {
-            color: '#ffffff',
+            color: '#ffffff', // White icons
           },
         },
       },
